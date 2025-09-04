77522f191aad759243a2111b62a1c544c8e5898f
Name <undrathi.deevena@gmail.com>
2025-09-04 09:36:08 +0000

    carbon foot print

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ import json
 import csv
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "https://carbon-footprint-calculator-2p71-dl977rlyo-deevena30s-projects.vercel.app"}},
+CORS(app, resources={r"/api/*": {"origins": "https://carbon-footprint-calculator-2p71.vercel.app/"}},
      supports_credentials=True)
 
 # Configurations
